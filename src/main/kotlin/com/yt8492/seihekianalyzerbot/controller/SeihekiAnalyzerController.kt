00c97dd8ab103548b488275d83d7550002ff3654
yt8492 <tmymyut2000@gmail.com>
2019-05-02 16:29:03 +0900

    fix controller

--- src/main/kotlin/com/yt8492/seihekianalyzerbot/controller/SeihekiAnalyzerController.kt
+++ src/main/kotlin/com/yt8492/seihekianalyzerbot/controller/SeihekiAnalyzerController.kt
@@ -8,20 +8,40 @@ import com.linecorp.bot.model.event.message.TextMessageContent
 import com.linecorp.bot.model.message.TextMessage
 import com.linecorp.bot.spring.boot.annotation.EventMapping
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
+import com.yt8492.seihekianalyzerbot.entity.Work
+import com.yt8492.seihekianalyzerbot.service.SeihekiAnalyzerService
 import org.springframework.beans.factory.annotation.Autowired
 
 @LineMessageHandler
-open class SeihekiAnalyzerController {
-
-    @Autowired
-    private lateinit var lineMessagingClient: LineMessagingClient
+open class SeihekiAnalyzerController(private val seihekiAnalyzerService: SeihekiAnalyzerService,
+                                     private val lineMessagingClient: LineMessagingClient) {
 
     @EventMapping
     fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>) {
         println("event: $event")
         val messageText = event.message.text
         val userId = event.source.userId
-        reply(event.replyToken, messageText)
+        when (messageText) {
+            "analyze" -> analyze(userId)
+            else -> reply(event.replyToken, messageText)
+        }
+    }
+
+    private fun analyze(userId: String) {
+        val works = seihekiAnalyzerService.findAll()
+        val tagCnt = mutableMapOf<String, Int>()
+        works.flatMap(Work::tags).forEach { tag ->
+            var cnt = tagCnt[tag] ?: 0
+            cnt++
+            tagCnt[tag] = cnt
+        }
+        val result = tagCnt.toList()
+                .sortedBy { it.second }
+                .take(10)
+                .mapIndexed { index, pair ->
+                    "%02d位 %2.2f%%: ${pair.first}".format(index + 1, (pair.second.toDouble() / tagCnt.size) * 100)
+                }.joinToString("\n")
+        pushMessage(userId, result)
     }
 
     private fun reply(token: String, text: String) {
