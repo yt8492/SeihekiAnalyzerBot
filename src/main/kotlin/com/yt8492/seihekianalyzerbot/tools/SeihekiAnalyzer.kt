13af3da9e0be0ad6a842be21ac63209e91239968
yt8492 <tmymyut2000@gmail.com>
2019-05-02 16:28:30 +0900

    fix SeihekiAnalyzer

--- src/main/kotlin/com/yt8492/seihekianalyzerbot/tools/SeihekiAnalyzer.kt
+++ src/main/kotlin/com/yt8492/seihekianalyzerbot/tools/SeihekiAnalyzer.kt
@@ -1,5 +1,6 @@
 package com.yt8492.seihekianalyzerbot.tools
 
+import com.yt8492.seihekianalyzerbot.service.SeihekiAnalyzerService
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
@@ -19,7 +20,7 @@ class SeihekiAnalyzer private constructor(private val loginCookies: Map<String, 
         }, urls, totalCnt)
     }
 
-    private fun getUrls(): List<String> {
+    fun getUrls(): List<String> {
         val historyCookies = JsoupUtils.requestByPost(URL_USER_BUY_HISTORY, cookies = loginCookies).cookies()
 
         val thisMonthUserBuyHistoryResult = JsoupUtils.requestByGet(URL_USER_BUY_HISTORY, cookies = historyCookies).parse()
@@ -35,13 +36,52 @@ class SeihekiAnalyzer private constructor(private val loginCookies: Map<String, 
                 .flatMap { d ->
                     d.getElementsByClass("work_name")
                             .map { e ->
-                                e.select("[href]").toString().split("\"")[1]
+                                try {
+                                    e.select("[href]").toString().split("\"")[1]
+                                } catch (error: IndexOutOfBoundsException) {
+                                    ""
+                                }
                             }
                 }
         return userBuyHistoryUrls
     }
 
     private fun analyzeTag(url: String, callback: Callback, failureCnt: Int = 0): Boolean {
+        try {
+            when {
+                failureCnt == 0 -> callback.onAnalyzingTag(url, false)
+                failureCnt < 5 -> callback.onAnalyzingTag(url, true)
+                else -> {
+                    callback.onTagAnalyzeFailure(url)
+                    return false
+                }
+            }
+            val voicePage = JsoupUtils.requestByGet(url).parse()
+            val rows = voicePage.getElementById("work_outline").select("tr")
+            val tags = rows.find { row ->
+                row.child(0).text() == "ジャンル"
+            }?.getElementsByClass("main_genre")?.select("[href]")?.text()?.split(" ".toRegex()) ?: listOf()
+            tags.forEach { tag ->
+                var cnt = genreCnt[tag]
+                cnt = if (cnt == null) {
+                    1
+                } else {
+                    cnt + 1
+                }
+                genreCnt[tag] = cnt
+            }
+            return true
+        } catch (e: SocketTimeoutException) {
+            return analyzeTag(url, callback, failureCnt + 1)
+        } catch (e: UnknownHostException) {
+            return analyzeTag(url, callback, failureCnt + 1)
+        } catch (e: Exception) {
+            return analyzeTag(url, callback, 5)
+        }
+    }
+
+    private fun analyzeTagWithDB(seihekiAnalyzerService: SeihekiAnalyzerService,
+                                 url: String, callback: Callback, failureCnt: Int = 0): Boolean {
         try {
             when {
                 failureCnt == 0 -> callback.onAnalyzingTag(url, false)
@@ -119,6 +159,15 @@ class SeihekiAnalyzer private constructor(private val loginCookies: Map<String, 
                 onLoginFailure()
                 null
             }
+        }
+
+        @JvmStatic
+        fun getTagsfromUrl(url: String): List<String> {
+            val voicePage = JsoupUtils.requestByGet(url).parse()
+            val rows = voicePage.getElementById("work_outline").select("tr")
+            return rows.find { row ->
+                row.child(0).text() == "ジャンル"
+            }?.getElementsByClass("main_genre")?.select("[href]")?.text()?.split(" ".toRegex()) ?: listOf()
         }
     }
 }
