1756219630f246973a73ebce76bfb09278a98c14
yt8492 <tmymyut2000@gmail.com>
2019-05-02 16:28:47 +0900

    add service

---
+++ src/main/kotlin/com/yt8492/seihekianalyzerbot/service/SeihekiAnalyzerService.kt
@@ -0,0 +1,9 @@
+package com.yt8492.seihekianalyzerbot.service
+
+import com.yt8492.seihekianalyzerbot.entity.Work
+
+interface SeihekiAnalyzerService {
+    fun findAll(): List<Work>
+    fun fetchWorkByUrl(url: String): Work
+    fun saveWork(work: Work)
+}
---
+++ src/main/kotlin/com/yt8492/seihekianalyzerbot/service/SeihekiAnalyzerServiceImpl.kt
@@ -0,0 +1,48 @@
+package com.yt8492.seihekianalyzerbot.service
+
+import com.yt8492.seihekianalyzerbot.entity.Tag
+import com.yt8492.seihekianalyzerbot.entity.Url
+import com.yt8492.seihekianalyzerbot.entity.UrlTag
+import com.yt8492.seihekianalyzerbot.entity.Work
+import com.yt8492.seihekianalyzerbot.property.SeihekiAnalyzerConfiguration
+import com.yt8492.seihekianalyzerbot.repository.TagRepository
+import com.yt8492.seihekianalyzerbot.repository.UrlRepository
+import com.yt8492.seihekianalyzerbot.repository.UrlTagRepository
+import com.yt8492.seihekianalyzerbot.tools.SeihekiAnalyzer
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class SeihekiAnalyzerServiceImpl(private val urlRepository: UrlRepository,
+                                 private val tagRepository: TagRepository,
+                                 private val urlTagRepository: UrlTagRepository,
+                                 private val seihekiAnalyzerConfiguration: SeihekiAnalyzerConfiguration)
+    : SeihekiAnalyzerService {
+    override fun findAll(): List<Work> {
+        val seihekiAnalyzer = SeihekiAnalyzer.login(seihekiAnalyzerConfiguration.id, seihekiAnalyzerConfiguration.password) ?: return listOf()
+        val allUrls = seihekiAnalyzer.getUrls()
+        val existsUrls = urlRepository.findAll().map(Url::url)
+        val notExistsUrls = allUrls.subtract(existsUrls)
+        val notExistsWorks = notExistsUrls.associate { it to SeihekiAnalyzer.getTagsfromUrl(it) }.map { Work(it.key, it.value) }
+        notExistsWorks.forEach { saveWork(it) }
+        val allWorks = urlRepository.findAll().map { url ->
+            fetchWorkByUrl(url.url)
+        }
+        return allWorks
+    }
+
+    override fun fetchWorkByUrl(url: String): Work = Work(url, urlTagRepository.findByUrlId(urlRepository.findByUrl(url).id!!).map { urlTag ->
+        tagRepository.findByIdOrNull(urlTag.tagId)!!.tag
+    })
+
+    override fun saveWork(work: Work) {
+        val url = Url(url = work.url)
+        val tags = work.tags.map { Tag(tag = it) }
+        urlRepository.save(url)
+        tagRepository.saveAll(tags)
+        val urlTags = tags.map { UrlTag(urlId = url.id!!, tagId = it.id!!) }
+        urlTagRepository.saveAll(urlTags)
+    }
+}
