2b18b7321a5d476e08b157f5788535647c1e7115
yt8492 <tmymyut2000@gmail.com>
2019-05-05 18:29:04 +0900

    refactor SeihekiAnalyzer

--- src/main/kotlin/com/yt8492/seihekianalyzerbot/tools/SeihekiAnalyzer.kt
+++ src/main/kotlin/com/yt8492/seihekianalyzerbot/tools/SeihekiAnalyzer.kt
@@ -62,12 +62,8 @@ class SeihekiAnalyzer private constructor(private val loginCookies: Map<String, 
                 row.child(0).text() == "ジャンル"
             }?.getElementsByClass("main_genre")?.select("[href]")?.text()?.split(" ".toRegex()) ?: listOf()
             tags.forEach { tag ->
-                var cnt = genreCnt[tag]
-                cnt = if (cnt == null) {
-                    1
-                } else {
-                    cnt + 1
-                }
+                var cnt = genreCnt[tag] ?: 0
+                cnt++
                 genreCnt[tag] = cnt
             }
             return true
@@ -77,42 +73,6 @@ class SeihekiAnalyzer private constructor(private val loginCookies: Map<String, 
             return analyzeTag(url, callback, failureCnt + 1)
         } catch (e: Exception) {
             return analyzeTag(url, callback, 5)
-        }
-    }
-
-    private fun analyzeTagWithDB(seihekiAnalyzerService: SeihekiAnalyzerService,
-                                 url: String, callback: Callback, failureCnt: Int = 0): Boolean {
-        try {
-            when {
-                failureCnt == 0 -> callback.onAnalyzingTag(url, false)
-                failureCnt < 5 -> callback.onAnalyzingTag(url, true)
-                else -> {
-                    callback.onTagAnalyzeFailure(url)
-                    return false
-                }
-            }
-            val voicePage = JsoupUtils.requestByGet(url).parse()
-            val rows = voicePage.getElementById("work_outline").select("tr")
-            val tags = rows.find { row ->
-                row.child(0).text() == "ジャンル"
-            }?.getElementsByClass("main_genre")?.select("[href]")?.text()?.split(" ".toRegex()) ?: listOf()
-            tags.forEach { tag ->
-                var cnt = genreCnt[tag]
-                cnt = if (cnt == null) {
-                    1
-                } else {
-                    cnt + 1
-                }
-                genreCnt[tag] = cnt
-            }
-            return true
-        } catch (e: SocketTimeoutException) {
-            return analyzeTag(url, callback, failureCnt + 1)
-        } catch (e: UnknownHostException) {
-            return analyzeTag(url, callback, failureCnt + 1)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return analyzeTag(url, callback, failureCnt + 1)
         }
     }
 
