3106dff98f8fdbc85b7c4b8d6e2f055e4b5c5a97
yt8492 <tmymyut2000@gmail.com>
2019-05-15 18:58:13 +0900

    check log

--- src/main/kotlin/com/yt8492/seihekianalyzerbot/controller/SeihekiAnalyzerController.kt
+++ src/main/kotlin/com/yt8492/seihekianalyzerbot/controller/SeihekiAnalyzerController.kt
@@ -49,6 +49,9 @@ open class SeihekiAnalyzerController(private val seihekiAnalyzerService: Seiheki
         }
         val result = tagCnt.toList()
                 .sortedByDescending { it.second }
+                .also { println(it.joinToString {
+                    "Tag: ${it.first}, Cnt: ${it.second}"
+                }) }
                 .take(10)
                 .mapIndexed { index, pair ->
                     "%2d位 %2.2f%%: ${pair.first}".format(index + 1, (pair.second.toDouble() / tagCnt.size) * 100)
