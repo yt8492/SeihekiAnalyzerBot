70e798797217eed97ab1a3e2f8dcad8669a76d59
yt8492 <tmymyut2000@gmail.com>
2019-06-26 15:50:28 +0900

    create tables

--- src/main/kotlin/com/yt8492/seihekianalyzerbot/SeihekianalyzerbotApplication.kt
+++ src/main/kotlin/com/yt8492/seihekianalyzerbot/SeihekianalyzerbotApplication.kt
@@ -1,6 +1,11 @@
 package com.yt8492.seihekianalyzerbot
 
+import com.yt8492.seihekianalyzerbot.exposed.table.LineUsers
+import com.yt8492.seihekianalyzerbot.exposed.table.Tags
+import com.yt8492.seihekianalyzerbot.exposed.table.WorkTags
+import com.yt8492.seihekianalyzerbot.exposed.table.Works
 import org.jetbrains.exposed.spring.SpringTransactionManager
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -15,6 +20,11 @@ import javax.sql.DataSource
 open class SeihekianalyzerbotApplication {
     @Bean
     fun transactionManager(dataSource: DataSource) = SpringTransactionManager(dataSource)
+
+    @Bean
+    fun createTables() {
+        SchemaUtils.create(Tags, Works, WorkTags, LineUsers)
+    }
 }
 
 fun main(args: Array<String>) {
